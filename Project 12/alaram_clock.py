74401cc2a871539721747c364f7cd3eee279807b
ayushpanchal26 <ayushpanchal2605@gmail.com>
2026-06-20 15:53:11 +0530

    new

---
+++ Project 12/alaram_clock.py
@@ -0,0 +1,26 @@
+#https://www.fesliyanstudios.com/royalty-free-sound-effects-download/alarm-203
+from playsound import playsound
+import time
+
+CLEAR = "\033[2]"
+CLEAR_AND_RETURN = "\033[H"
+
+
+def alarm(seconds):
+    time_elapsed = 0 
+
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN} Alarm will sound in: {minutes_left : 02d} : {seconds_left : 02d}")
+        playsound("C:\\Project 21\\Python-Projects\\Project 12\\alarm.mp3.mp3")
+
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = minutes * 60 + seconds
+alarm(10)
